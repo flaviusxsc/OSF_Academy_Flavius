96cf9bcdfdca0db2c3eedccbaa190a6aec9e5462
Flavius <44696922+flaviusxsc@users.noreply.github.com>
2020-09-07 16:50:34 +0300

    Setting up gulp for SCSS

--- gulpfile.js
+++ gulpfile.js
@@ -1,2 +1,11 @@
-‘use strict’; 
-var gulp = require(‘gulp’);
+'use strict';
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var concat = require('gulp-concat');
+sass.compiler = require('node-sass');
+gulp.task('sass', function () {
+   return gulp.src('./sass/**/*.scss')
+   .pipe(concat('style.scss'))
+   .pipe(sass().on('error', sass.logError))
+   .pipe(gulp.dest('./css/'));
+});
