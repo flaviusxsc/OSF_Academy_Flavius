c06ceaa813a2c4d5d01d3b9ff454e389b4d516f4
Flavius <44696922+flaviusxsc@users.noreply.github.com>
2020-09-09 22:13:13 +0300

    09.09.2020

--- gulpfile.js
+++ gulpfile.js
@@ -8,4 +8,8 @@ gulp.task('sass', function () {
    .pipe(concat('style.scss'))
    .pipe(sass().on('error', sass.logError))
    .pipe(gulp.dest('./css/'));
+}); 
+
+gulp.task('sass:watch', function () {
+   gulp.watch('./sass/**/*.scss', gulp.series('sass'));
 });
