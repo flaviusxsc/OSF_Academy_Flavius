379f8e8672d227642c4baded4f2597fa2c3300bb
Flavius <44696922+flaviusxsc@users.noreply.github.com>
2020-09-23 23:27:57 +0300

    cookie and local storage

--- js/main.js
+++ js/main.js
@@ -26,4 +26,18 @@ let integerheart = 0;
 addheart.addEventListener ('click', function(){
 integerheart += 1;
 intheart.innerHTML = integerheart;
-})
+}) 
+// Cookie Policy 
+
+const cookieContainer = document.querySelector(".cookie-container");
+const cookieButton = document.querySelector(".cookie-btn");
+
+cookieButton.addEventListener("click",() => {
+  cookieContainer.classList.remove("active");
+  localStorage.setItem("cookieBannerDisplayed","true")
+});
+
+setTimeout( () =>{
+  if(!localStorage.getItem("cookieBannerDisplayed"))
+  cookieContainer.classList.add("active")
+}, 2000)
