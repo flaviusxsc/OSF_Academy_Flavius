8b5f0ebf1b851930172b9a580a53c0bf581135e1
Flavius <44696922+flaviusxsc@users.noreply.github.com>
2020-09-23 17:50:35 +0300

    shopping cart increment

--- js/main.js
+++ js/main.js
@@ -3,5 +3,27 @@ var date = new Date();
 document.getElementById("year").innerHTML = date.getFullYear();
 
 
+// Shopping increment by plus
 
+let addplus = document.getElementById('increment-plus');
 
+let int = document.getElementById('number-bag');
+
+let integer = 0;
+
+addplus.addEventListener ('click', function(){
+integer += 1;
+int.innerHTML = integer;
+})
+
+// Shopping increment by heart 
+let addheart = document.getElementById('increment-heart');
+
+let intheart = document.getElementById('number-heart');
+
+let integerheart = 0;
+
+addheart.addEventListener ('click', function(){
+integerheart += 1;
+intheart.innerHTML = integerheart;
+})
