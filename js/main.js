ca1f7ab3695e4780575631d5fed0c5dceacb13a3
Flavius <44696922+flaviusxsc@users.noreply.github.com>
2020-09-26 22:08:17 +0300

    products-page

--- js/main.js
+++ js/main.js
@@ -53,3 +53,39 @@ addspecial.addEventListener ('click', function(){
 integerspecial += 1;
 intcart.innerHTML = integerspecial;
 }) ;
+
+// Thumbnails cards  - nu merge
+ let thumbnails = document.getElementsByClassName('thumbnail');
+
+ let activeImages = document.getElementsByClassName('active');
+
+ for (var i=0; i < thumbnails.length; i++) {
+   
+   thumbnails[i].addEventListener('mouseover', function(){
+
+     if (activeImages.length > 0) {
+         activeImages[0].classList.remove('active')
+     }
+
+     this.classList.add('active')
+     document.getElementById('product-main-image').src = this.src;
+
+   })
+ }
+
+ // Read More Button 
+ function myFunction() {
+  var dots = document.getElementById("dots");
+  var moreText = document.getElementById("more");
+  var btnText = document.getElementById("myBtn");
+
+  if (dots.style.display === "none") {
+    dots.style.display = "inline";
+    btnText.innerHTML = "Read more";
+    moreText.style.display = "none";
+  } else {
+    dots.style.display = "none";
+    btnText.innerHTML = "Read less";
+    moreText.style.display = "inline";
+  }
+}
