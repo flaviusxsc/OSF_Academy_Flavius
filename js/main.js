c01d44b216648b25b102d1444d1e76eb372ec2eb
Flavius <44696922+flaviusxsc@users.noreply.github.com>
2020-09-27 23:21:31 +0300

    27.09.2020

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-// console.log("RUNNING")
+// Get the year
 var date = new Date();
 document.getElementById("year").innerHTML = date.getFullYear();
 
@@ -92,5 +92,28 @@ intcart.innerHTML = integerspecial;
 // Function show password 
 
 function showPassword() {
-  $("#defaultForm-pass").attr("type","");
-}
+  var x = document.getElementById("defaultForm-pass");
+  if (x.type === "password") {
+    x.type = "text";
+  } else {
+    x.type = "password";
+  }
+} 
+
+// Counter products-page 
+$(document).ready(function(){
+  $('button').click(function(e){
+      var button_classes, value = +$('.counter').val();
+      button_classes = $(e.currentTarget).prop('class');        
+      if(button_classes.indexOf('up_count') !== -1){
+          value = (value) + 1;            
+      } else {
+          value = (value) - 1;            
+      }
+      value = value < 0 ? 0 : value;
+      $('.counter').val(value);
+  });  
+  $('.counter').click(function(){
+      $(this).focus().select();
+  });
+});
